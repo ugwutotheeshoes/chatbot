a10646d7803b4db9a92223fa4038077e585351d5
Emmanuel Ugwu <>
2021-07-12 21:23:50 +0100

    Create response.js

---
+++ response.js
@@ -0,0 +1,79 @@
+const userTexts = [
+    ["hi", "hey", "hello", "good morning", "good afternoon", "good day"],
+    ["how are you", "how is life", "how are things", "how are you doing", "are you doing good",
+     "are you fine", "how is your day going", "how is your day", "what's up", "whats up", "you good"],
+    ["what are you doing", "what is going on", "what is up", "what's up", "whats up",
+     "you good"],
+    ["how is your day", "how was your day", "how did your day go", "how's your day going",
+    "how's your day", "hows your day", "how's your day going", "how is your day going"],
+    ["how old are you", "are you old"],
+    ["who are you", "are you human", "are you bot", "are you human or bot"],
+    ["who created you", "who made you", "were you created"],
+    [
+      "your name please",
+      "your name",
+      "may i know your name",
+      "what is your name",
+      "what call yourself",
+      "What are you called",
+      "What do i call you",
+      "do you have a name",
+      "tell me your name"
+    ],
+    ["i love you", "i like you"],
+    ["happy", "good", "fun", "wonderful", "fantastic", "cool", "nice", "lovely"],
+    ["bad", "bored", "tired", "sad"],
+    ["help me", "tell me story", "tell me joke", "im bored"],
+    ["ah", "yes", "ok", "okay", "nice"],
+    ["bye", "good bye", "goodbye", "see you later"],
+    ["what should i eat today", "what do i cook today"],
+    ["bro"],
+    ["what", "why", "how", "where", "when"],
+    ["no","not sure","maybe","no thanks"],
+    [""],
+    ["haha","ha","lol","hehe","funny","joke","lmao"],
+    ["im ok", "im good", "im okay", "im fine", "good"]
+  ]
+
+const botReplies = [
+    ["Hello!", "Hi!", "Hey!", "Hi there!","Howdy"],
+    [
+      "Fine... and you?",
+      "Pretty well, and you?",
+      "Fantastic, and you?"
+    ],
+    [
+      "Nothing much",
+      "About to go to sleep",
+      "I'm just chilling",
+      "I don't know actually"
+    ],
+    ["Been good so far"],
+    ["I am infinite"],
+    ["I am just a bot", "I am a bot. What are you?"],
+    ["The one true God, JavaScript"],
+    ["I am nameless", "I don't have a name"],
+    ["I love you too", "Me too"],
+    ["Have you ever felt bad?", "Glad to hear it"],
+    ["Why?", "Why? You shouldn't!", "Try watching TV"],
+    ["What about?", "Once upon a time... that's the end of my story"],
+    ["Tell me a story", "Tell me a joke", "Tell me about yourself"],
+    ["Bye", "Goodbye", "See you later"],
+    ["Sushi", "Pizza"],
+    ["Bro!"],
+    ["Great question"],
+    ["That's ok","I understand","What do you want to talk about?"],
+    ["Please say something :("],
+    ["Haha!","Good one!"],
+    ["that's nice", "that's good", "nice", "okay"]
+  ]
+    
+const alternative = [
+    "Same",
+    "Go on...",
+    "Bro...",
+    "Try again",
+    "I'm listening...",
+    "I don't understand :/"
+  ]
+  
